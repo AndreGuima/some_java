384cff256c9b95542f771c1ed24cbd0bd4242bb0
AndreGuima <andrepaivaguimaraes@gmail.com>
2022-05-20 12:12:45 -0300

    added new calculator

--- test/hellotest.java
+++ test/hellotest.java
@@ -6,19 +6,26 @@ import static org.junit.Assert.fail;
 
 public class hellotest {
 
+    private Calculator calculator;
+
+    @Before
+    void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testNothing() {
     }
 
     @Test
-    public void testWillAlwaysFail() {
-        fail("This is An Error");
+    void testMultiply() {
+        assertEquals( "Regular multiplication should work", calculator.multiply(4,5), 20);
     }
 
     @Test
-    public void testWillAlwaysSuccess() {
-        success("This is An Success");
+    void testMultiplyWithZero() {
+        assertEquals("Multiple with zero should be zero",0,  calculator.multiply(0,5));
+        assertEquals("Multiple with zero should be zero", 0, calculator.multiply(5,0));
     }
-
 
 }
